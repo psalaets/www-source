504bbb8016f8a1ebd0044ac920f55c99546ab4ce
Paul Salaets <psalaets@gmail.com>
2020-06-12 20:54:29 -0400

    tailwind includes purgecss under the hood now

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,4 @@
 const tailwindcss = require('tailwindcss')
-const purgecss = require('@fullhuman/postcss-purgecss')
 const cssnano = require('cssnano')
 const autoprefixer = require('autoprefixer')
 const postcssImport = require('postcss-import');
@@ -10,24 +9,6 @@ const plugins = [
   autoprefixer,
   cssnano,
 ];
-
-if (process.env.NODE_ENV === 'production') {
-  plugins.push(
-    purgecss({
-      content: ['./_site/**/*.html'],
-      extractors: [
-        {
-          extractor: class TailwindExtractor {
-            static extract(content) {
-              return content.match(/[A-Za-z0-9-_:\/]+/g) || []
-            }
-          },
-          extensions: ['css', 'html', 'vue'],
-        },
-      ]
-    })
-  )
-}
 
 module.exports = {
   plugins
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [
+    './lib/**/*.js',
+    './src/**/*.njk',
+    './src/**/*.js',
+    './src/**/*.css',
+    './src/**/*.md',
+    '.eleventy.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
